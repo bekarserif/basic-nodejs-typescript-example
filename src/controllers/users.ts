468314245977c0d8a901d38874fa2cafff59b7c0
bekarserif <48939900+bekarserif@users.noreply.github.com>
2023-04-30 17:57:41 +0300

    DeleteUser and isOwner Middleware

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -1,11 +1,31 @@
 import { Request, Response } from 'express';
 
-import { getUsers } from '../db/users';
+import { getUsers, getUserById, deleteUserById } from '../db/users';
 
 export const getAllUsers = async (req: Request, res: Response) => {
   try {
     const users = await getUsers();
     return res.status(200).json(users);
+  } catch (error) {
+    console.log(error);
+    return res.sendStatus(400);
+  }
+};
+
+export const deleteUser = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+
+    if (!id) {
+      return res.sendStatus(400);
+    }
+    const deletedUser = await getUserById(id);
+    if (!deletedUser) {
+      return res.sendStatus(400);
+    }
+
+    await deleteUserById(deletedUser._id.toString());
+    return res.status(200).json(deletedUser).end();
   } catch (error) {
     console.log(error);
     return res.sendStatus(400);
