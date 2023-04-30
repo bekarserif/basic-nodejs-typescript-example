11b0a0bafd8c7f53db85467dd9e12a3f9407f761
bekarserif <48939900+bekarserif@users.noreply.github.com>
2023-04-30 18:06:40 +0300

    Update User

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -31,3 +31,23 @@ export const deleteUser = async (req: Request, res: Response) => {
     return res.sendStatus(400);
   }
 };
+
+export const updateUser = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+    const { username } = req.body;
+
+    if (!username) {
+      return res.sendStatus(400);
+    }
+    const user = await getUserById(id);
+
+    user.username = username;
+    await user.save();
+
+    return res.status(200).json(user).end();
+  } catch (error) {
+    console.log(error);
+    return res.sendStatus(400);
+  }
+};
